e3b24643a87507fcaf555afaf7abdfacc716f2b7
desmond9986 <desmond9986@gmail.com>
2021-01-19 20:25:50 +0800

    Milestone 1: generate the shell of an application

--- app/src/main/java/com/example/ass3_3011712/MainActivity.java
+++ app/src/main/java/com/example/ass3_3011712/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.ass3_3011712;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btn.getText().toString().equals("Start Tracker"))
+                    btn.setText("Stop Tracker");
+                else{
+                    btn.setText("Start Tracker");
+                    Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
